156fab630149f7ed9ba57981c0d12367bea099e4
saul9402 <saul_harry_@hotmail.com>
2018-10-10 23:56:53 -0500

    Componentes anidados
    Se usa componentes anidados y tambien la propiedad template en ves de templateUrl para el html

--- src/app/fechaactual/fechaactual.component.ts
+++ src/app/fechaactual/fechaactual.component.ts
@@ -4,7 +4,9 @@ import {Component} from '@angular/core'; //Se importa el decorador Component
 @Component({
     //aqui se ponen los metadatos que definen este componente
     selector: 'app-fechaactual', //nombre de la etiqueta donde se renderiza el componente
-    templateUrl: './fechaactual.component.html',  //La ruta enla que va aestar la plantila
+    //templateUrl: './fechaactual.component.html',  //La ruta enla que va aestar la plantila
+    template: `<p>Madrid, España, {{hoy | date: 'd/M/y H:mm'}} </p>
+    <app-copyright></app-copyright>`, //si el fragmento de codigo que se pretede usar en el html es muy pequeño se puede sustituir por esto, directamente en el componente. Podrias hasta eliminar el atchivo html. Y se pueden inclir componentes como el <app-copyright></app-copyright> dentro de este mismo
     styleUrls: ['./fechaactual.component.css'] //son lo estilos que se podran aplicar a el template declarado arriba
 })
 
