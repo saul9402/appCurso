771124b46c5aade5bb99809da5b2f752c824b21f
saul9402 <saul_harry_@hotmail.com>
2018-10-12 23:29:41 -0500

    se agrega directiva de else con un metodo dentro del ngIf

--- src/app/ejdirectivangif/ejdirectivangif.component.ts
+++ src/app/ejdirectivangif/ejdirectivangif.component.ts
@@ -7,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EjdirectivangifComponent implements OnInit {
 
-  nombre: string;
+  //nombre: string;
+  capital: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setResultado(){
+    return this.capital==="Madrid" ? true : false;
   }
 
 }
