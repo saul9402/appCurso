a0a7651caf04f6aa7337a52072518e30f44c9e22
saul9402 <saul_harry_@hotmail.com>
2018-10-22 23:33:05 -0500

    Pipe i18nSelect
    ayuda a seleccionar un valor con base en alguna propiedad y la pinta en el html

--- src/app/fechaactual/fechaactual.component.ts
+++ src/app/fechaactual/fechaactual.component.ts
@@ -7,7 +7,8 @@ import {Component} from '@angular/core'; //Se importa el decorador Component
     //templateUrl: './fechaactual.component.html',  //La ruta enla que va aestar la plantila
     //Par el pipe de numeros se pone primero el numero de enteros a mostrar, luego el numeor minimo de 
     //decimales un guión y al final el numero maximo de decimales
-    template: `<p>El resultado es: {{ resultado | number: '3.2-2' }}</p>`,
+    template: //`<p>La cotizacion actual del dólar es de {{ dolareuro | currency: 'EUR': true }}</p>`,
+    ` <p>{{ sexo | i18nSelect: encabezamiento}} {{ nombre }}</p> `,
     //uppercase y lowercase sin opciones, es lo unico que hacen
     /*`<p>{{ ciudad | uppercase }}, {{hoy | date: 'd/M/y H:mm'}} </p>
     <app-copyright></app-copyright>`,*/ //si el fragmento de codigo que se pretede usar en el html es muy pequeño se puede sustituir por esto, directamente en el componente. Podrias hasta eliminar el atchivo html. Y se pueden inclir componentes como el <app-copyright></app-copyright> dentro de este mismo
@@ -20,5 +21,8 @@ export class FechaactualComponent{ //se exporta el componente para que se pueda 
    // hoy = new Date(); //esta es lo logica ; se declara una variable llamada 'hoy'
 
     //ciudad: string = 'Madrid';
-    resultado: number= 1.148;   
+    // dolareuro: number = 0.88;
+    nombre: string = 'Carlos';
+    sexo: string = 'hombre';
+    encabezamiento: any = { 'hombre':'Estimado', 'mujer':'Estimada' };
 }
